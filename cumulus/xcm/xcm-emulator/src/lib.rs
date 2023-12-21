869f581a5b0c3e0082d343fd6f8b0a788f507f28
Agusrodri <agusrodriguez2456@gmail.com>
2023-12-21 13:55:41 -0800

    adapt xcm-emulator macros

--- cumulus/xcm/xcm-emulator/src/lib.rs
+++ cumulus/xcm/xcm-emulator/src/lib.rs
@@ -37,8 +37,8 @@ pub use cumulus_primitives_core::AggregateMessageOrigin as CumulusAggregateMessa
 pub use frame_support::{
 	assert_ok,
 	sp_runtime::{
-		traits::{Convert, Dispatchable, Header as HeaderT, Zero},
-		Digest, DispatchResult,
+		traits::{Dispatchable, Header as HeaderT},
+		Digest, DigestItem, DispatchResult,
 	},
 	traits::{
 		EnqueueMessage, ExecuteOverweightError, Get, Hooks, OnFinalize, OnIdle, OnInitialize,
@@ -54,6 +54,8 @@ pub use pallet_balances::AccountData;
 pub use pallet_message_queue;
 pub use pallet_timestamp::Call as TimestampCall;
 pub use sp_arithmetic::traits::Bounded;
+pub use sp_consensus_aura::AURA_ENGINE_ID;
+pub use sp_consensus_slots::Slot;
 pub use sp_core::{
 	crypto::get_public_from_string_or_panic, parameter_types, sr25519, storage::Storage, Pair,
 };
@@ -190,6 +192,7 @@ pub trait Network {
 		para_id: u32,
 		relay_parent_number: u32,
 		parent_head_data: HeadData,
+		relay_slot: u64,
 	) -> ParachainInherentData;
 	fn send_horizontal_messages<I: Iterator<Item = (ParaId, RelayBlockNumber, Vec<u8>)>>(
 		to_para_id: u32,
@@ -665,9 +668,8 @@ macro_rules! decl_test_parachains {
 				}
 
 				fn new_block() {
-					use $crate::{
-						Dispatchable, Chain, Convert, TestExt, Zero,
-					};
+					use $crate::{Chain, HeadData, Network, Hooks, Encode, Parachain, TestExt,
+						Slot, AURA_ENGINE_ID, Digest, DigestItem};
 
 					let para_id = Self::para_id().into();
 
@@ -688,30 +690,26 @@ macro_rules! decl_test_parachains {
 							.clone()
 						);
 
-						// Initialze `System`.
-						let digest = <Self as Parachain>::DigestProvider::convert(block_number);
-						<Self as Chain>::System::initialize(&block_number, &parent_head_data.hash(), &digest);
+						let digests = <Self as Chain>::System::digest();
 
-						// Process `on_initialize` for all pallets except `System`.
-						let _ = $runtime::AllPalletsWithoutSystem::on_initialize(block_number);
+						let slot = digests
+							.convert_first(|item| item.pre_runtime_try_to::<Slot>(&AURA_ENGINE_ID))
+							.unwrap_or_default();
 
-						// Process parachain inherents:
+						let slot = u64::from(slot);
 
-						// 1. inherent: cumulus_pallet_parachain_system::Call::set_validation_data
-						let set_validation_data: <Self as Chain>::RuntimeCall = $crate::ParachainSystemCall::set_validation_data {
-							data: N::hrmp_channel_parachain_inherent_data(para_id, relay_block_number, parent_head_data),
-						}.into();
-						$crate::assert_ok!(
-							set_validation_data.dispatch(<Self as Chain>::RuntimeOrigin::none())
-						);
+						let new_slot_digest: Digest = Digest {
+							logs: vec![
+								DigestItem::PreRuntime(AURA_ENGINE_ID, (slot + 1u64).encode()),
+							],
+						};
 
-						// 2. inherent: pallet_timestamp::Call::set (we expect the parachain has `pallet_timestamp`)
-						let timestamp_set: <Self as Chain>::RuntimeCall = $crate::TimestampCall::set {
-							// We need to satisfy `pallet_timestamp::on_finalize`.
-							now: Zero::zero(),
-						}.into();
-						$crate::assert_ok!(
-							timestamp_set.dispatch(<Self as Chain>::RuntimeOrigin::none())
+						<Self as Chain>::System::initialize(&block_number, &parent_head_data.hash(), &new_slot_digest);
+						<<Self as Parachain>::ParachainSystem as Hooks<$crate::BlockNumberFor<Self::Runtime>>>::on_initialize(block_number);
+
+						let _ = <Self as Parachain>::ParachainSystem::set_validation_data(
+							<Self as Chain>::RuntimeOrigin::none(),
+							N::hrmp_channel_parachain_inherent_data(para_id, relay_block_number, parent_head_data, ((slot + 1))),
 						);
 					});
 				}
@@ -1156,6 +1154,7 @@ macro_rules! decl_test_networks {
 					para_id: u32,
 					relay_parent_number: u32,
 					parent_head_data: $crate::HeadData,
+					relay_slot: u64
 				) -> $crate::ParachainInherentData {
 					let mut sproof = $crate::RelayStateSproofBuilder::default();
 					sproof.para_id = para_id.into();
@@ -1170,6 +1169,7 @@ macro_rules! decl_test_networks {
 						}
 
 						sproof.included_para_head = parent_head_data.clone().into();
+						sproof.current_slot = relay_slot.into();
 
 						sproof
 							.hrmp_channels
