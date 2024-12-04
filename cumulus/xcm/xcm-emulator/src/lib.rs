758746d3657be932d4e7f8257add8e37ed27f296
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-12-04 16:06:54 +0100

    Fix xcm emulator, use generic AggregateMessageOrigin

--- cumulus/xcm/xcm-emulator/src/lib.rs
+++ cumulus/xcm/xcm-emulator/src/lib.rs
@@ -50,7 +50,7 @@ pub use sp_consensus_aura::AURA_ENGINE_ID;
 pub use sp_core::{parameter_types, sr25519, storage::Storage, Pair};
 pub use sp_crypto_hashing::blake2_256;
 pub use sp_io::TestExternalities;
-pub use sp_runtime::BoundedSlice;
+pub use sp_runtime::{traits::Convert, BoundedSlice};
 pub use sp_tracing;
 
 // Cumulus
@@ -64,7 +64,8 @@ pub use cumulus_test_relay_sproof_builder::RelayStateSproofBuilder;
 pub use pallet_message_queue::{Config as MessageQueueConfig, Pallet as MessageQueuePallet};
 pub use parachains_common::{AccountId, Balance};
 pub use polkadot_primitives;
-pub use polkadot_runtime_parachains::inclusion::{AggregateMessageOrigin, UmpQueueId};
+pub use polkadot_runtime_parachains::inclusion::{UmpQueueId};
+pub use polkadot_runtime_parachains::inclusion::Config as InclusionConfig;
 
 // Polkadot
 pub use polkadot_parachain_primitives::primitives::RelayChainBlockNumber;
@@ -1415,15 +1416,17 @@ where
 	}
 }
 
+type GenericAMO<T> = <<T as Chain>::Runtime as InclusionConfig>::AggregateMessageOrigin;
+
 pub struct DefaultRelayMessageProcessor<T>(PhantomData<T>);
 // Process UMP messages on the relay
 impl<T> ProcessMessage for DefaultRelayMessageProcessor<T>
 where
 	T: RelayChain,
-	T::Runtime: MessageQueueConfig,
+	T::Runtime: MessageQueueConfig + InclusionConfig,
 	<<T::Runtime as MessageQueueConfig>::MessageProcessor as ProcessMessage>::Origin:
-		PartialEq<AggregateMessageOrigin>,
-	MessageQueuePallet<T::Runtime>: EnqueueMessage<AggregateMessageOrigin> + ServiceQueues,
+		PartialEq<GenericAMO<T>>,
+	MessageQueuePallet<T::Runtime>: EnqueueMessage<GenericAMO<T>> + ServiceQueues,
 {
 	type Origin = ParaId;
 
@@ -1435,7 +1438,7 @@ where
 	) -> Result<bool, ProcessMessageError> {
 		MessageQueuePallet::<T::Runtime>::enqueue_message(
 			msg.try_into().expect("Message too long"),
-			AggregateMessageOrigin::Ump(UmpQueueId::Para(para)),
+			<T::Runtime as InclusionConfig>::GetAggregateMessageOrigin::convert(UmpQueueId::Para(para)),
 		);
 		MessageQueuePallet::<T::Runtime>::service_queues(Weight::MAX);
 
@@ -1446,10 +1449,10 @@ where
 impl<T> ServiceQueues for DefaultRelayMessageProcessor<T>
 where
 	T: RelayChain,
-	T::Runtime: MessageQueueConfig,
+	T::Runtime: MessageQueueConfig + InclusionConfig,
 	<<T::Runtime as MessageQueueConfig>::MessageProcessor as ProcessMessage>::Origin:
-		PartialEq<AggregateMessageOrigin>,
-	MessageQueuePallet<T::Runtime>: EnqueueMessage<AggregateMessageOrigin> + ServiceQueues,
+		PartialEq<GenericAMO<T>>,
+	MessageQueuePallet<T::Runtime>: EnqueueMessage<GenericAMO<T>> + ServiceQueues,
 {
 	type OverweightMessageAddress = ();
 
