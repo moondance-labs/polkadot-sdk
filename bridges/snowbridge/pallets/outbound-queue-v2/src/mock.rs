e5a229682d63fe1c74aafb4a32a198acee4129df
Agustín Rodriguez <agusrodriguez2456@gmail.com>
2025-09-23 06:15:18 -0300

    Snowbridge V2: Add `OnNewCommitment` hook to outbound-queue pallet (#8053)
    ## Description
    
    This PR adds a simple hook to `snowbridge-pallet-outbound-queue-v2`
    which allows to perform actions whenever there is a new commitment in
    this pallet.
    
    ---------
    
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- bridges/snowbridge/pallets/outbound-queue-v2/src/mock.rs
+++ bridges/snowbridge/pallets/outbound-queue-v2/src/mock.rs
@@ -144,6 +144,7 @@ impl crate::Config for Test {
 	type EthereumNetwork = EthereumNetwork;
 	type RewardKind = BridgeReward;
 	type DefaultRewardKind = DefaultMyRewardKind;
+	type OnNewCommitment = ();
 	#[cfg(feature = "runtime-benchmarks")]
 	type Helper = Test;
 }
