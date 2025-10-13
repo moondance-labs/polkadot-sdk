18aebf2d78daa27b460f997fe374a9aa4d263993
Parth <desaiparth08@gmail.com>
2025-10-13 11:24:15 +0400

    Tanssi polkadot stable2503 with fulu (#17)
    * Snowbridge - Adds Fulu hardfork (#9938)
    
    Adds Fulu hardfork version. No other onchain changes required.
    
    Fulu activation on Sepolia is 14 October, Mainnet 3 December.
    
    ---------
    
    Co-authored-by: Adrian Catangiu <adrian@parity.io>
    (cherry picked from commit c2231626893533a91754384b62f9595ee97662d3)
    
    * fix test
    
    ---------
    
    Co-authored-by: Clara van Staden <claravanstaden64@gmail.com>
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/bridge_to_ethereum_config.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/bridge_to_ethereum_config.rs
@@ -251,6 +251,10 @@ parameter_types! {
 			version: hex!("05000000"),
 			epoch: 80000000000, // setting to a future epoch for local testing to remain on Deneb.
 		},
+		fulu: Fork {
+			version: hex!("90000075"),
+			epoch: 272640, // https://notes.ethereum.org/@bbusa/fusaka-bpo-timeline
+		},
 	};
 }
 
@@ -279,7 +283,11 @@ parameter_types! {
 		},
 		electra: Fork {
 			version: hex!("90000074"),
-			epoch: 222464, // https://github.com/ethereum/EIPs/pull/9322/files
+			epoch: 222464,
+		},
+		fulu: Fork {
+			version: hex!("90000075"),
+			epoch: 272640, // https://notes.ethereum.org/@bbusa/fusaka-bpo-timeline
 		},
 	};
 }
